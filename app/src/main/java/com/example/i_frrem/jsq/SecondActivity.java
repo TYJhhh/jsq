73256eb2efda71e3a9f93eeb60e36884a85a0eb4
TYJhhh <1227794413@qq.com>
2019-01-27 20:04:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/i_frrem/jsq/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.i_frrem.jsq;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+    }
+}
